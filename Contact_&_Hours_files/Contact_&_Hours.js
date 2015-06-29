e22d1e098430afb05ec2440c8a72add47236b9c2
Adam Harris <aharris88@users.noreply.github.com>
2015-06-29 12:17:43 -0600

    Start new design

--- Contact_&_Hours_files/Contact_&_Hours.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20120714
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Contact_&_Hours_files/Contact_&_HoursMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();fixupIECSS3Opacity('id2');performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
